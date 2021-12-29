c7dd603b63b0331379efdf149837219f45cc85dd
ibrahim Yıldız <ibrahim0015@gmail.com>
2021-12-29 14:36:09 +0300

    login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,32 +1,41 @@
 import { Form, Input, Button, Result } from 'antd';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { AppState } from '../store/actions/reducers';
+import { login } from '../store/actions/userActions';
+import { LoginForm } from '../types/user';
 import api from '../utils/api';
 import showError from "../utils/showError";
+import showSuccess from '../utils/showSuccess';
 
 function Login() {
 
     const navi = useNavigate();
     const location = useLocation();
+    const dispatch = useDispatch();
 
-    console.log(location);
+    const { data, loading, error } = useSelector((state: AppState) => state.user);
 
-
-    const onFinish = async (values: any) => {
-        console.log('Success:', values);
-        try {
-            await api.post("/users/login", values);
-            navi('/');
-        }
-        catch (error) {
-            console.log({ error });
-        }
-
-    };
-
-    const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', { errorInfo });
-        showError(errorInfo);
+    const onFinish = (values: LoginForm) => {
+        dispatch(login(values));
     }
+
+    useEffect(() => {
+        error && showError(error)
+    }, [error]);
+
+
+    useEffect(() => {
+        data.username && showSuccess("You have successfuly logged in!");
+    }, [data.username]);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            navi("/");
+        }
+    }, [data]);
 
     return (
         <Form
@@ -35,20 +44,20 @@ function Login() {
             wrapperCol={{ span: 16 }}
             initialValues={{ remember: true }}
             onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
+            /*    onFinishFailed={onFinishFailed} */
             autoComplete="off"
         >
             <h2 style={{ textAlign: "center", marginBottom: 40 }}>
                 Please Login
             </h2>
-            {location.state?.newSignUp && 
-            (  <Result
-                status="success"
-                title="You successfully signed up!"
-                subTitle="Please login using your credentials."          
-            />)  
+            {location.state?.newSignUp &&
+                (<Result
+                    status="success"
+                    title="You successfully signed up!"
+                    subTitle="Please login using your credentials."
+                />)
             }
-          
+
             <Form.Item
                 label="Username"
                 name="username"
--- src/store/actions/reducers/userReducer.ts
+++ src/store/actions/reducers/userReducer.ts
@@ -1,13 +1,21 @@
 import { User, UserAction, UserState } from "../../../types/user";
 
-const defaulState:UserState={
-    data:{} as User,
-    loading:false,
-    error:""    
+const defaulState: UserState = {
+    data: {} as User,
+    loading: false,
+    error: ""
 }
 
-const userReducer = (state:UserState=defaulState,action:UserAction)=>{
-    return state;
+const userReducer = (state: UserState = defaulState, action: UserAction) => {
+    switch (action.type) {
+        case "LOGIN_START":
+            return { ...state, loading: true, error: "" };
+        case "LOGIN_SUCCESS":
+            return { ...state, loading: false, data: action.payload }
+        case "LOGIN_ERROR":
+            return {...state,loading:false,error:"Login failed."}
+        default: return state;
+    }
 }
 
 export default userReducer;
